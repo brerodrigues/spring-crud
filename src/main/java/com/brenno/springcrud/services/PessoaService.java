4e10e180b3eda0bd1b442d0058695705d6723403
Brenno Rodrigues <brerodrigues@users.noreply.github.com>
2021-01-19 19:17:28 -0300

    Faz com que o parametro do constructor seja final para evitar comportamentos indesejados

--- src/main/java/com/brenno/springcrud/services/PessoaService.java
+++ src/main/java/com/brenno/springcrud/services/PessoaService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 public class PessoaService {
 
-    private PessoaRepository pessoaRepository;
+    private final PessoaRepository pessoaRepository;
 
     public PessoaService(PessoaRepository pessoaRepository) {
         this.pessoaRepository = pessoaRepository;
